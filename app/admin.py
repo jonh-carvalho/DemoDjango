4415a27eb79c66759fbf04cecc523d9231de74f3
Jonh Carvalho <jonh.carvalho@professores.ibmec.edu.br>
2024-10-18 11:20:15 -0300

    update

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Employee
+from .models import Employee,Department
 
-admin.site.register(Employee)  # Register your models here.
+admin.site.register(Employee) 
+admin.site.register(Department) # Register your models here.
